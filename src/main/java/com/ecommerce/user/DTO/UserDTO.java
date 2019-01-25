1161fb26d5ecaf0fb4b03b53b0b6ecc502f783d2
sahilkothari <sahilkothari114@gmail.com>
2019-01-25 17:29:56 +0530

    Login Validations

--- src/main/java/com/ecommerce/user/DTO/UserDTO.java
+++ src/main/java/com/ecommerce/user/DTO/UserDTO.java
@@ -6,8 +6,26 @@ public class UserDTO {
     private String emailId;
     private String password;
     private String address;
+    private String message;
+    private int status;
 
     public UserDTO() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 
     public long getUserId() {
--- src/main/java/com/ecommerce/user/controller/UserController.java
+++ src/main/java/com/ecommerce/user/controller/UserController.java
@@ -34,10 +34,38 @@ public class UserController {
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/signIn", method = RequestMethod.POST)
     public ResponseEntity<UserDTO> signIn(@RequestBody UserDTO userDTO){
-        if(Objects.isNull(userDTO) || Objects.isNull(userDTO.getName()) || Objects.isNull(userDTO.getEmailId()) || Objects.isNull(userDTO.getPassword()) ){
+        UserDTO userDTO1 = new UserDTO();
+        if(Objects.isNull(userDTO) ){
+            userDTO1.setMessage("Name, EmailId or Password can not be empty.");
+            userDTO1.setStatus(400);
             return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
         }
-        User user = new User();
+        else if(userDTO.getEmailId()==null){
+            userDTO1.setMessage("EmailId can not be empty.");
+            userDTO1.setStatus(400);
+            return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
+        }
+        else if(userDTO.getPassword()==null){
+            userDTO1.setMessage("Password can not be empty.");
+            userDTO1.setStatus(400);
+            return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
+        }
+        else if(userDTO.getName()==null){
+            userDTO1.setMessage("Name can not be empty.");
+            userDTO1.setStatus(400);
+            return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
+        }
+        else if(userDTO.getAddress()==null){
+            userDTO1.setMessage("Address can not be empty.");
+            userDTO1.setStatus(400);
+            return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
+        }
+        User user = userService.findByEmailId(userDTO.getEmailId());
+        if (Objects.isNull(user)){
+            userDTO1.setMessage("Email Id already exists.");
+            userDTO1.setStatus(400);
+            return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
+        }
         BeanUtils.copyProperties(userDTO,user);
         try {
             user.setPassword(passwordHash.createHash(userDTO.getPassword()));
@@ -46,68 +74,67 @@ public class UserController {
         } catch (InvalidKeySpecException e) {
             e.printStackTrace();
         }
-        User signedInUser=new User();
+        User signedInUser;
         try {
             signedInUser = userService.save(user);
         }catch (Exception e){
             return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
-
         }
         if (Objects.isNull(signedInUser)){
             return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
         }
         userDTO=new UserDTO();
         BeanUtils.copyProperties(user,userDTO);
-
         return new ResponseEntity<UserDTO>(userDTO,HttpStatus.CREATED);
     }
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.POST, value = "/login")
-    public ResponseEntity<String> login(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<UserDTO> login(@RequestBody UserDTO userDTO) {
         LOGGER.info("Received a request for login");
         ResponseEntity<String> response = null;
         User user = new User();
         BeanUtils.copyProperties(userDTO,user);
+        if(Objects.isNull(userDTO)){
+            userDTO.setMessage("EmailId or Password can not be empty.");
+            userDTO.setStatus(400);
+            return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
+        }
+        else if (userDTO.getEmailId()==null){
+            userDTO.setMessage("EmailId can not be empty.");
+            userDTO.setStatus(400);
+            return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
+        }
+        else if (userDTO.getPassword()==null) {
+            userDTO.setMessage("Password can not be empty.");
+            userDTO.setStatus(400);
+            return new ResponseEntity<UserDTO>(userDTO, HttpStatus.BAD_REQUEST);
+        }
+        user = userService.findByEmailId(user.getEmailId());
+        if(Objects.isNull(user)){
+            userDTO.setMessage("EmailId Does Not Exist");
+            userDTO.setStatus(400);
+            return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
+        }
+         boolean isLoginSuccessful = false;
         try {
-            if (!Objects.isNull(user) && !Objects.isNull(user.getEmailId()) && !Objects.isNull(user.getPassword())) {
-                final String username = user.getEmailId();
-                final User ExistingUser = userService.findByEmailId(username);
-                JSONObject exceptionJson = new JSONObject();
-                    exceptionJson.put("message", "Invalid login. Please check your name and password.");
-
-                if (Objects.isNull(ExistingUser)) {
-                    LOGGER.info("Login failed.");
-                    response = new ResponseEntity<>(exceptionJson.toString(), HttpStatus.UNAUTHORIZED);
-                } else {
-                    final boolean isLoginSuccessful = passwordHash.validatePassword(user.getPassword(),ExistingUser.getPassword());
-
-                    if (isLoginSuccessful) {
-                        JSONObject userJson = new JSONObject();
-                        userJson.put("userId", ExistingUser.getUserId())
-                                .put("name", ExistingUser.getName())
-                                .put("emailId", ExistingUser.getEmailId());
-                        response = new ResponseEntity<>(userJson.toString(), HttpStatus.OK);
-                        LOGGER.info("Login successful");
-                    } else {
-                        response = new ResponseEntity<>(exceptionJson.toString(), HttpStatus.UNAUTHORIZED);
-                        LOGGER.info("Login failed.");
-                    }
-                }
-            } else {
-                JSONObject exceptionJson = new JSONObject();
-                exceptionJson.put("message", "Email or password cannot be null.");
-                response = new ResponseEntity<>(exceptionJson.toString(), HttpStatus.BAD_REQUEST);
-                LOGGER.info("Login failed.");
-            }
-        } catch (JSONException e) {
-            LOGGER.error("Error occurred while creating JSON object");
-            response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            isLoginSuccessful = passwordHash.validatePassword(userDTO.getPassword(),user.getPassword());
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (InvalidKeySpecException e) {
             e.printStackTrace();
         }
-        return response;
+        BeanUtils.copyProperties(user,userDTO);
+
+        if (isLoginSuccessful){
+            userDTO.setMessage("Succussful");
+            return new ResponseEntity<>(userDTO, HttpStatus.OK);
+        }
+        else {
+            userDTO.setMessage("Login failed");
+            userDTO.setStatus(400);
+            return new ResponseEntity<UserDTO>(userDTO,HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     /**
